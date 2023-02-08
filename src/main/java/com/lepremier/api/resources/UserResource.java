a978932d8ec176fb561696e598215ba79c61ade7
Gabriel Dantas <gabrieldantaslp@gmail.com>
2023-02-08 15:20:03 -0300

    Service layer, component registration

--- src/main/java/com/lepremier/api/resources/UserResource.java
+++ src/main/java/com/lepremier/api/resources/UserResource.java
@@ -1,20 +1,34 @@
 package com.lepremier.api.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lepremier.api.entities.User;
+import com.lepremier.api.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+	@Autowired
+	private UserService userService;
+	
 	@GetMapping
-	public ResponseEntity<User> getUser() {
-		User user = new User(1, "gabriel", "gabriel@mail.com", "9999", "123");
-		return ResponseEntity.ok().body(user);
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = userService.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Integer id) {
+		User obj = userService.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 	
 }
---
+++ src/main/java/com/lepremier/api/services/UserService.java
@@ -0,0 +1,27 @@
+package com.lepremier.api.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lepremier.api.entities.User;
+import com.lepremier.api.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> findAll() {
+		return userRepository.findAll();
+	}
+	
+	public User findById(Integer id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.get();
+	}
+
+}
