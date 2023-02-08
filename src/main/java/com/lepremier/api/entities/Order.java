80f5d5422c4f2ba295ff5d0969a75b340ed035e2
Gabriel Dantas <gabrieldantaslp@gmail.com>
2023-02-08 15:59:15 -0300

    OrderStatus enum

--- src/main/java/com/lepremier/api/entities/Order.java
+++ src/main/java/com/lepremier/api/entities/Order.java
@@ -13,8 +13,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
+import com.lepremier.api.entities.enums.OrderStatus;
 
 @Entity
 @Table(name = "tb_order")
@@ -32,6 +31,7 @@ public class Order implements Serializable {
 			timezone = "GMT")
 	private Instant moment;
 	
+	private Integer orderStatus;
 	
 	@ManyToOne
 	@JoinColumn(name = "client_id")
@@ -40,10 +40,11 @@ public class Order implements Serializable {
 	public Order() {
 	}
 
-	public Order(Integer id, Instant moment, User client) {
+	public Order(Integer id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);;
 		this.client = client;
 	}
 
@@ -61,6 +62,16 @@ public class Order implements Serializable {
 
 	public void setMoment(Instant moment) {
 		this.moment = moment;
+	}
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {			
+			this.orderStatus = orderStatus.getCode();
+		}
 	}
 
 	public User getClient() {
