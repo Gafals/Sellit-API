17082208b68fe3902d5ddb51d1149d3239320094
Gabriel Dantas <gabrieldantaslp@gmail.com>
2023-02-08 20:41:16 -0300

    Exception handling to findById

--- src/main/java/com/lepremier/api/services/UserService.java
+++ src/main/java/com/lepremier/api/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.lepremier.api.entities.User;
 import com.lepremier.api.repositories.UserRepository;
+import com.lepremier.api.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Integer id) {
 		Optional<User> obj = userRepository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User userCreate(User obj) {
